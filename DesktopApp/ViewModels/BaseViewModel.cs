2789948e3856c502f32cfd4fe2a6fbd4d9884ca9
Максим Денисов <max_denisow68@mail.ru>
2020-10-08 21:24:18 +0300

    Initialized base class for viewmodels, add view`s folder

---
+++ DesktopApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DesktopApp.ViewModels
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal bool Set<T>(ref T field, T value, [CallerMemberName]string prop = "")
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(prop);
+            return true;
+        }
+
+        internal void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
