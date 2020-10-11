1c4a46f7c63cf6533cdd7fd86263f772d3b64af1
kharin <kharin@samsonpost.ru>
2020-10-11 13:48:01 +0300

    CR-17 Map controller

---
+++ API/Controllers/IndexController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        public string ApiIsRuning()
+        {
+            return "API Running...";
+            //Todo: maybe add AspNetCore.RouteAnalyzer
+        }
+
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -16,6 +16,7 @@ namespace API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +28,11 @@ namespace API
             }
 
             app.UseRouting();
-            app.UseEndpoints(endpoints=>{
-                endpoints.MapControllerRoute("default",
-                    "/{controller}/{action}/id?");
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
             });
-
         }
     }
 }
