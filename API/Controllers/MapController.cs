05abfbd297bb5f555b42a45a8e31f44ba64bb34c
kharin <kharin@samsonpost.ru>
2020-10-11 13:48:01 +0300

    CR-17 Map controller

---
+++ API/Controllers/MapController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class MapController : ControllerBase
+    {
+
+        [HttpPost("MapCreateRequest")]
+        public async Task<ActionResult<API.Models.Map>> MapCreateRequest(API.Models.Map mapItem)
+        {
+            return mapItem;
+        }
+
+        [HttpPost("MapUpdateRequest")]
+        public async Task<ActionResult<API.Models.Map>> MapUpdateRequest(API.Models.Map mapItem)
+        {
+            return mapItem;
+        }
+
+        [HttpPost("MapGetResponse")]
+        public async Task<ActionResult<API.Models.Map>> MapGetResponse([FromBody] Guid id)
+        {
+            return new API.Models.Map(id);
+        }
+
+        [HttpPost("MapImageUploadRequest")]
+        public async Task<ActionResult<API.Models.Image>> MapImageUploadRequest([FromBody] Guid id)
+        {
+            return new API.Models.Image(id);
+        }
+
+        [HttpPost("MapImageIdGetResponse")]
+        public async Task<ActionResult<API.Models.Image>> MapImageIdGetResponse([FromBody] Guid id)
+        {
+            return new API.Models.Image(id);
+        }
+
+    }
+}
---
+++ API/Models/Map.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Map
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Map(Guid Id, string Name = "")
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
