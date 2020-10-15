41909a0e79d5ed4bba7e788c9df93611e52988a2
EkaterinaTikhomirova <plumpfat1998@gmail.com>
2020-10-15 16:42:13 +0300

    CR-22 Upload and get image controller

---
+++ Repository/CityRouteContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class CityRouteContext : DbContext
+    {
+        public CityRouteContext(DbContextOptions<CityRouteContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            new MapImageBuild(modelBuilder.Entity<MapImage>());
+        }
+    }
+}
