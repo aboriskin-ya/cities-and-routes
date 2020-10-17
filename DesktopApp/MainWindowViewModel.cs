0a5b8d4b70493b7626dbc96fea2deb6ef15bf499
Екатерина Тихомирова <plumpfat1998@gmail.com>
2020-10-17 18:50:05 +0300

    CR-23 design new window and add new command

---
+++ DesktopApp/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace DesktopApp
+{
+    class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            ShowCreateMapDialogCommand = new SimpleCommand(d => ShowDialog());
+        }
+
+        public ICommand ShowCreateMapDialogCommand { get; }
+
+        private void ShowDialog()
+        {
+            CreateMapDialog cmd = new CreateMapDialog();
+            cmd.Show();
+        }
+    }
+}
