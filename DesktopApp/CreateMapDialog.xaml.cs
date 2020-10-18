da0bd0920eb0de5bab38f0960ef9c3f60f9e9db5
Екатерина Тихомирова <plumpfat1998@gmail.com>
2020-10-18 22:49:37 +0300

    add CreateMapViewModel and drag-drop command

--- DesktopApp/CreateMapDialog.xaml.cs
+++ DesktopApp/CreateMapDialog.xaml.cs
@@ -1,22 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DesktopApp
 {
+    public enum AllowExtensions { jpg, png };
     public partial class CreateMapDialog : Window
     {
         public CreateMapDialog()
         {
             InitializeComponent();
         }
+        //private void MapImage_Drop(object sender, DragEventArgs e)
+        //{
+        //    if(e.Data.GetDataPresent(DataFormats.FileDrop))
+        //    {
+        //        string[] dropPath = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+        //        if( Enum.IsDefined(typeof(AllowExtensions), Path.GetExtension(dropPath[0]).Trim('.')) )
+        //        {
+        //            //mapName.Text = Path.GetFileNameWithoutExtension(dropPath[0]);
+        //            newMap.Source = new BitmapImage(new Uri(Path.GetFullPath(dropPath[0])));
+        //            newMap.Width = scroll.Width;
+        //            newMap.Stretch = Stretch.None;
+        //            scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+        //        }
+
+        //    }
+        //}
     }
+    
 }
---
+++ DesktopApp/CreateMapViewModel.cs
@@ -0,0 +1,67 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GongSolutions.Wpf.DragDrop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DesktopApp
+{
+    public class CreateMapViewModel: ViewModelBase, IDropTarget
+    {
+        private string mapName = "new map";
+        public string MapName
+        {
+            get { return mapName; }
+            set
+            {
+                mapName = value;
+                RaisePropertyChanged();
+            }
+        }   
+
+        public ICommand CreateCommand { get; }
+
+        private void CreateNewMap()
+        {
+            
+            mapName = "fuck the system";
+            RaisePropertyChanged("MapName");
+        }
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+            dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+
+            var dataObject = dropInfo.Data as IDataObject;
+            
+            if (dataObject != null && dataObject.GetDataPresent(DataFormats.FileDrop))
+            {
+                dropInfo.Effects = DragDropEffects.Link;
+            }
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            var dataObject = dropInfo.Data as DataObject;
+            string[] dropPath = dataObject.GetData(DataFormats.FileDrop, true) as string[];
+
+            if (Enum.IsDefined(typeof(AllowExtensions), Path.GetExtension(dropPath[0]).Trim('.')))
+            {
+                MapName = Path.GetFileNameWithoutExtension(dropPath[0]);
+                
+            }
+        }
+
+        public CreateMapViewModel()
+        {
+            CreateCommand = new RelayCommand(CreateNewMap);
+        }
+       
+    }
+}
