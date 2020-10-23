1658cb937fabeeaa290d94c954dc5aeb6939b72d
Dima <sarancev.vrn@gmail.com>
2020-10-23 21:19:19 +0300

    Corrected mistakes

--- DataAccess/RouteBuild.cs
+++ DataAccess/RouteBuild.cs
@@ -10,9 +10,6 @@ namespace DataAccess
         {
             entityBuilder.HasKey(r => r.Id);
             entityBuilder.Property(r => r.Distance).IsRequired();
-            entityBuilder.HasOne(r => r.Map).WithMany().HasForeignKey("MapId").HasConstraintName("FK1");
-            //entityBuilder.HasOne(r => r.FirstCity).WithMany().HasForeignKey("FirstCityId").HasConstraintName("FK1");
-            //entityBuilder.HasOne(r => r.SecondCity).WithMany().HasForeignKey("SecondCityId").HasConstraintName("FK1");
         }
     }
 }
--- Repository/Migrations/20201021115804_InitialCreate.cs
+++ Repository/Migrations/20201021115804_InitialCreate.cs
@@ -47,6 +47,56 @@ namespace Repository.Migrations
                 name: "IX_Map_ImageId",
                 table: "Map",
                 column: "ImageId");
+
+            migrationBuilder.CreateTable(
+                name: "Route",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateOnUTC = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedOnUTC = table.Column<DateTimeOffset>(nullable: false),
+                    Distance = table.Column<int>(nullable: false),
+                    MapId = table.Column<Guid>(nullable: false),
+                    FirstCityId = table.Column<Guid>(nullable: false),
+                    SecondCityId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Map", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK1",
+                        column: x => x.FirstCityId,
+                        principalTable: "Map",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK2",
+                        column: x => x.SecondCityId,
+                        principalTable: "Map",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK3",
+                        column: x => x.MapId,
+                        principalTable: "Map",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Route_MapId",
+                table: "Route",
+                column: "MapId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Route_FirstCityId",
+                table: "Route",
+                column: "FirstCityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Route_SecondCityId",
+                table: "Route",
+                column: "SecondCityId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
