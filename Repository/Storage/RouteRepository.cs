ece61fca652c873520d1d1acc30bfead7ccc412e
Dima <sarancev.vrn@gmail.com>
2020-10-23 19:48:49 +0300

    Added RouteController

---
+++ Repository/Storage/RouteRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Storages
+{ 
+    public class RouteRepository : Repository<Route>, IRouteRepository
+    {
+        public RouteRepository(CityRouteContext context) : base(context)
+        {
+        }
+
+        public new Route Get(Guid id)
+        {
+            return _entity.Include(p => p.Map).SingleOrDefault(p => p.Id == id);
+        }
+    }
+}
