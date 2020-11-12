5460ba6d696fd0bd5279866dc02a5aafa832a925
kekoses <max_denisow68@mail.ru>
2020-11-12 21:11:05 +0300

    fix all

--- Service/Services/TravelSalesmanNearestNeighbor.cs
+++ Service/Services/TravelSalesmanNearestNeighbor.cs
@@ -3,6 +3,7 @@ using Service.PathResolver;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Service.Services.Interfaces
 {
@@ -46,6 +47,7 @@ namespace Service.Services.Interfaces
                 _minWeightValue = int.MaxValue;
                 if (_sequence.Count == graph.Vertices.Count && _currentVertex.IsUnvisited == false)
                 {
+                    _result += graph.GetEdge(_sequence.Last().ToString(),_sequence.First().ToString()).EdgeWeight;
                     _allVisited = true;
                 }
             }
