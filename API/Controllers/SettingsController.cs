bc410474e60935de333f42b9427edbe5661f4c1b
Екатерина Тихомирова <plumpfat1998@gmail.com>
2020-11-29 13:18:44 +0300

    CR-43 Integrate settings dialog with API and WPF map (controller)

--- API/Controllers/SettingsController.cs
+++ API/Controllers/SettingsController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
             _service = service;
         }
 
-        [ProducesResponseType(typeof(IEnumerable<SettingsCreateDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<SettingsGetDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -37,7 +37,7 @@ namespace API.Controllers
             return Ok(settings);
         }
 
-        [ProducesResponseType(typeof(SettingsCreateDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(SettingsGetDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -55,7 +55,7 @@ namespace API.Controllers
             return Ok(settings);
         }
 
-        [ProducesResponseType(typeof(SettingsCreateDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(SettingsGetDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -73,18 +73,18 @@ namespace API.Controllers
             return Ok(settings);
         }
 
-        [ProducesResponseType(typeof(SettingsCreateDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(SettingsGetDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost]
         public IActionResult Post([FromBody] SettingsCreateDTO settingsDTO)
         {
-            _service.CreateSettings(settingsDTO);
-            return Ok(settingsDTO);
+            var dto = _service.CreateSettings(settingsDTO);
+            return Ok(dto);
         }
 
-        [ProducesResponseType(typeof(SettingsCreateDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(SettingsGetDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
