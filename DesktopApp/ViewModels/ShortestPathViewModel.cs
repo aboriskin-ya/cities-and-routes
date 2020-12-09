d8573d402252409c0a080e2f69d665a18a1c52ca
kekoses <59687939+kekoses@users.noreply.github.com>
2020-12-09 21:42:56 +0300

    Update ShortestPathViewModel.cs

--- DesktopApp/ViewModels/ShortestPathViewModel.cs
+++ DesktopApp/ViewModels/ShortestPathViewModel.cs
@@ -97,8 +97,7 @@ namespace DesktopApp.ViewModels
             builder.Append($"\nProcess` duration: {shortestPath.ProcessDuration}\n" +
                             $"Calculated distance: {shortestPath.FinalDistance}\n");
             ConsoleResult += builder.ToString();
-            
-        }
+            }
 
         private bool OnCanCalculateShortestPathExecute(object p) => true;
 
