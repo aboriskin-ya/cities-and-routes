602b0c183e9998aa48c774afdcfec0b5237906dc
kekoses <max_denisow68@mail.ru>
2020-12-12 20:14:59 +0300

    shifted authentication info to app settings

--- DesktopApp/APIInteraction/APIClient.cs
+++ DesktopApp/APIInteraction/APIClient.cs
@@ -7,12 +7,12 @@ namespace DesktopApp.APIInteraction
     public class APIClient
     {
         public static HttpClient Client { get; set; }
-        public static void InitializeClient(string baseUri)
+        public static void InitializeClient(string baseUri, string authType, string authInfo)
         {
             Client = new HttpClient();
             Client.BaseAddress = new Uri(baseUri);
             Client.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes("Admin:Cities_Pass")));
+                new AuthenticationHeaderValue(authType, Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authInfo)));
         }
     }
 }
